8f3200d7d6ea6944528c97c3a0fc7236203dee1b
jessand77 <46941395+jessand77@users.noreply.github.com>
2021-01-30 17:38:31 +0100

    Add new link color

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Hello Codecademy!
+          Hello Codecademy! How are you?
         </p>
         <a
           className="App-link"
